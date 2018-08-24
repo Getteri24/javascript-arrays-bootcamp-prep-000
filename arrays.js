563854d202ab54df1e86185ea67cea03fb6f8214
t bryant <trbryant1981@gmail.com>
2018-08-24 18:02:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,10 +40,9 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1);
-  array = array
-  return array;
+  return array.slice(0, array.length - 1);
 }
+
 
 
 
