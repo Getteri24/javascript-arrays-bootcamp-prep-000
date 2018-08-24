60e4404d92a6a0888cdcaea8ff1c7cd0bf1a3d62
t bryant <trbryant1981@gmail.com>
2018-08-24 17:30:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,4 +23,5 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+console.log(accessElementInArray(chocolateBars[1]));
 
